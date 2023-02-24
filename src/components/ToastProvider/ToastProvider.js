d3bec62e5ef97079fa63936e48c2dd122cc60fc5
Jimmy Chen <ccking37@gmail.com>
2023-02-24 15:15:46 +0800

    Finish exercise 6

--- src/components/ToastProvider/ToastProvider.js
+++ src/components/ToastProvider/ToastProvider.js
@@ -1,21 +1,13 @@
 import React from 'react';
-
+import useEscapeKey from '../../hooks/use-escape-key.hook';
 export const ToastContext = React.createContext();
 
 function ToastProvider({ children }) {
 	const [toasts, setToasts] = React.useState([]);
 
-	React.useEffect(() => {
-		function handleKeyDown(event) {
-			if (event.code === 'Escape') {
-				setToasts([]);
-			}
-		}
-		window.addEventListener('keydown', handleKeyDown);
-		return () => {
-			window.removeEventListener('keydown', handleKeyDown);
-		};
-	}, []);
+	useEscapeKey(() => {
+		setToasts([]);
+	});
 
 	function createToast(message, variant) {
 		setToasts([
